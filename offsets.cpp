4eb84354ab75a42104fcc3b65ce80f8b1ee64ff5
ILLUSION <76027489+GLX-ILLUSION@users.noreply.github.com>
2023-05-10 15:23:54 -0300

    Update offsets.cpp

--- offsets.cpp
+++ offsets.cpp
@@ -9,42 +9,60 @@
 template <typename T>
 void readValue(const nlohmann::json& src, T& dest)
 {
-	if (!src.is_null())
-	{
-		if (src.is_string())
-		{
-			std::string value_str = src.get<std::string>();
-			if (value_str.substr(0, 2) == "0x") // hex prefix
-			{
-				dest = static_cast<T>(std::stoul(value_str.substr(2), nullptr, 16));
-			}
-			else
-			{
-				dest = static_cast<T>(std::stoul(value_str, nullptr, 10));
-			}
-		}
-		else
-		{
-			dest = src.get<T>();
-		}
-	}
+    if (!src.is_null())
+    {
+        if (src.is_string())
+        {
+            std::string value_str = src.get<std::string>();
+            if (value_str.substr(0, 2) == "0x") // hex prefix
+            {
+                dest = static_cast<T>(std::stoul(value_str.substr(2), nullptr, 16));
+            }
+            else
+            {
+                dest = static_cast<T>(std::stoul(value_str, nullptr, 10));
+            }
+        }
+        else
+        {
+            dest = src.get<T>();
+        }
+    }
 }
 
+template <>
+void readValue<unsigned long long>(const nlohmann::json& src, unsigned long long& dest)
+{
+    if (!src.is_null())
+    {
+        if (src.is_string())
+        {
+            std::string value_str = src.get<std::string>();
+            if (value_str.substr(0, 2) == "0x") // hex prefix
+            {
+                dest = std::stoull(value_str.substr(2), nullptr, 16);
+            }
+            else
+            {
+                dest = std::stoull(value_str, nullptr, 10);
+            }
+        }
+        else
+        {
+            dest = src.get<unsigned long long>();
+        }
+    }
+}
 
 void offsets::initialize()
 {
-	std::ifstream input_file{ "offsets.json" };
-	if (!input_file.good())
-		throw std::invalid_argument("Invalid offsets.json file");
+    std::ifstream input_file{ "offsets.json" };
+    if (!input_file.good())
+        throw std::invalid_argument("Invalid offsets.json file");
+    nlohmann::json json;
+    input_file >> json;
 
-	nlohmann::json json;
-	input_file >> json;
-
-	readValue(json["offsets"]["VM_XOR_BASE"], offsets::VM_XOR_BASE);
-	readValue(json["offsets"]["VM_XOR_KEY"], offsets::VM_PTR);
-	readValue(json["offsets"]["VM_PTR"], offsets::VM_XOR_KEY);
-	
-
-
-
+    readValue(json["offsets"]["VM_XOR_BASE"], offsets::VM_XOR_BASE);
+    readValue(json["offsets"]["VM_PTR"], offsets::VM_PTR);
+    readValue(json["offsets"]["VM_XOR_KEY"], offsets::VM_XOR_KEY);
 }
